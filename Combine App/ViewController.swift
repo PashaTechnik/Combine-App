31202a71117a2f313a22f911676e7e00b26f536b
Pasha Technik <pacha120431@gmail.com>
2021-09-30 20:42:41 +0300

    init Combine app

--- Combine App/ViewController.swift
+++ Combine App/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var purchaseButtton: UIButton!
+    private var cancellable: AnyCancellable?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        cancellable = NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: priceTextField)
+            .map { $0.object as? UITextField }
+            .compactMap { $0?.text}
+            .map { str -> UIColor in
+                if let number = Double(str){
+                    if number > 10 {
+                        return .green
+                    }
+                }
+                return .red
+            }
+            .assign(to: \.tintColor, on: purchaseButtton)
+        
+        
+        
     }
-
-
 }
 
